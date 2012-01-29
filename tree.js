822a49ee7743c6226c593d9f1822778561f7a0e2
Chris McCormick <chris@mccormick.cx>
2012-01-29 11:29:55 +0800

    Only draw trees if they are in the field of view.

--- tree.js
+++ tree.js
@@ -1,9 +1,15 @@
 function Tree(gs, world, treenum, position) {
-	this.priority = world.iso.w2s(position)[1];
 	var sprite = (new Sprite(["center", "bottom"], {"default": [["res/img/tree-" + treenum + ".png", 1]]})).action("default");
 	
+	this.update = function(c) {
+		this.priority = world.iso.w2s(position)[1];
+	}
+	
 	this.draw = function(c) {
-		world.shadow.draw(c, world.iso.w2s(position));
-		sprite.draw(c, world.iso.w2s(position));
+		var spos = world.iso.w2s(position);
+		if (spos[0] >= -sprite.width && spos[1] >= -sprite.height && spos[0] < gs.width + sprite.width && spos[1] < gs.height + sprite.height) {
+			world.shadow.draw(c, spos);
+			sprite.draw(c, spos);
+		}
 	}
 }
